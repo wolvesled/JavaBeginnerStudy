2d1e1c0078f69df0bc7719a128dce4eb2d145a6e
wolvesled <wolvesled@gmail.com>
2013-08-29 17:01:22 +0800

    No automatic conversion from double to long

---
+++ DtoL.java
@@ -0,0 +1,12 @@
+// *** This program will not complie. ***
+class LtoD {
+  public static void main(String args[]) {
+    long L;
+    double D;
+    
+    D = 100123285.0;
+    L = D; // Illegal!!!
+    
+    System.out.println("L and D: " + L + " " + D);
+  }
+}
