50e11ed9231ff7f23ee5c3a11f1da825dab908b2
wolvesled <wolvesled@gmail.com>
2013-08-29 19:30:41 +0800

    Demonstrate the switch

--- SwitchDemo.java
+++ SwitchDemo.java
@@ -1,20 +1,23 @@
 // Demonstrate the switch.
 class SwitchDemo {
 	public static void main(String args[]) {
-		int 1;
+		int i;
 		
 		for(i = 0; i < 10; i++)
 			switch(i) {
 				case 0:
 					System.out.println("i is zero");
-					break
-				case 1;
+					break;
+				case 1:
 					System.out.println("i is one");
 					break;
-				case 2;
+				case 2:
+					System.out.println("i is two");
+					break;
+				case 3:
 					System.out.println("i is three");
 					break;
-				case 3;
+				case 4:
 					System.out.println("i is four");
 					break;
 				default:
