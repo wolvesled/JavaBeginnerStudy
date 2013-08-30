26ee8e9cc2fa5e201a8db13fd5a377203b4722da
wolvesled <wolvesled@gmail.com>
2013-08-30 21:04:57 +0800

    An improved help system using do-while

--- Help2.java
+++ Help2.java
@@ -17,40 +17,40 @@ class Help2 {
 			System.out.print("Choose one: ");
 			do {
 				choice = (char) System.in.read();
-			} while(choice < '1' | choice > '5');
-			
-			System.out.println("\n");
-			
-			switch(choice) {
-				case '1':
-					System.out.println("The if:\n");
-					System.out.println("if(condition) statement;");
-					System.out.println("else statement;");
-					break;
-				case '2':
-					System.out.println("The switch:\n");
-					System.out.println("switch(expression) {");
-					System.out.println("  case constant:");
-					System.out.println("    statement sequence");
-					System.out.println("    break;");
-					System.out.println("  // ...");
-					System.out.println("}");
-					break;
-				case '3':
-					System.out.println("The for:\n");
-					System.out.println("for(initialization; condition; iteration) statement;");
-					break;
-				case '4':
-					System.out.println("The while:\n");
-					System.out.println("while(condition) statement;");
-					break;
-				case '5':
-					System.out.println("The do-while:\n");
-					System.out.println("do {");
-					System.out.println("  statement;");
-					System.out.println("} while (condition);");
-					break;
-			}
+			} while(choice == '\n' | choice == '\r');
+		} while(choice < '1' | choice > '5');
+		
+		System.out.println("\n");
+		
+		switch(choice) {
+			case '1':
+				System.out.println("The if:\n");
+				System.out.println("if(condition) statement;");
+				System.out.println("else statement;");
+				break;
+			case '2':
+				System.out.println("The switch:\n");
+				System.out.println("switch(expression) {");
+				System.out.println("  case constant:");
+				System.out.println("    statement sequence");
+				System.out.println("    break;");
+				System.out.println("  // ...");
+				System.out.println("}");
+				break;
+			case '3':
+				System.out.println("The for:\n");
+				System.out.println("for(initialization; condition; iteration) statement;");
+				break;
+			case '4':
+				System.out.println("The while:\n");
+				System.out.println("while(condition) statement;");
+				break;
+			case '5':
+				System.out.println("The do-while:\n");
+				System.out.println("do {");
+				System.out.println("  statement;");
+				System.out.println("} while (condition);");
+				break;
 		}
 	}
 }
