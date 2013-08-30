9ebf34d80906f0ff47f1abfa33ca1f7546b1d390
wolvesled <wolvesled@gmail.com>
2013-08-30 21:13:43 +0800

    Use nested loops

---
+++ FindFac.java
@@ -0,0 +1,14 @@
+/*
+	Use nested loops to find factors of number between 2 and 100.
+*/
+class FindFac {
+	public static void main(Straing args[]) {
+	
+		for(int i=2; i <= 100; i++) {
+			System.out.print("Factors of " + i + ": ");
+			for(int j = 2; j < i; j++)
+				if((i%j) == 0) System.out.print(j + " ");
+			System.out.println();
+		}
+	}
+}
