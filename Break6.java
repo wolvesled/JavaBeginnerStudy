51d3aab9105ef8e569a3014722ed63f931797a6b
wolvesled <wolvesled@gmail.com>
2013-08-30 20:19:18 +0800

    Where you put a label is important

--- Break6.java
+++ Break6.java
@@ -5,20 +5,20 @@ class Break6 {
 		
 // here, put label before for statement.
 stop1:		for(x=0; x < 5; x++) {
-				for(y = 0; y < 5; y++) {
-					if(y == 2) break stop1;
-					System.out.println("x and y: " + x + " " + y);
-				}
+			for(y = 0; y < 5; y++) {
+				if(y == 2) break stop1;
+				System.out.println("x and y: " + x + " " + y);
 			}
-			System.out.println();
+		}
+		System.out.println();
 			
 // now, put label immediately before {
-			for(x=0; x < 5; x++)
-stop2:			{
-				for(y = 0; y < 5; y++) {
-					if(y == 2) break stop2;
-					System.out.println("x and y: " + x + " " + y);
-				}
+		for(x=0; x < 5; x++)
+stop2:		{
+			for(y = 0; y < 5; y++) {
+				if(y == 2) break stop2;
+				System.out.println("x and y: " + x + " " + y);
 			}
+		}
 	}
 }
