c15e6329e30c22b7c435a4634bab518ba6157b47
wolvesled <wolvesled@gmail.com>
2013-09-01 14:50:04 +0800

    Introduce String

--- StringDemo.java
+++ StringDemo.java
@@ -3,7 +3,7 @@ class StringDemo {
 	public static void main(String args[]) {
 		// declare strings in various ways
 		String str1 = new String("Java strings are objects.");
-		String str2 = "They are constructed various ways.");
+		String str2 = "They are constructed various ways.";
 		String str3 = new String(str2);
 		
 		System.out.println(str1);
