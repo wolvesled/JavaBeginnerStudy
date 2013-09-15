c306c7f834c447b73778d8b4524ad1b01fada426
wolvesled <wolvesled@gmail.com>
2013-09-15 17:52:54 +0800

    Demonstrate System.out.write()
    http://ideone.com/ETtHHG

---
+++ WriteDemo.java
@@ -0,0 +1,11 @@
+// Demonstrate System.out.write().
+
+class WriteDemo {
+	public static void main(String args[]) {
+		int b;
+		
+		b = 'X';
+		System.out.write(b);
+		System.out.write('\n');
+	}
+}
