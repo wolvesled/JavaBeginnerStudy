c418bf10e7db235bb04f408bfe65c0941a28b8fe
wolvesled <wolvesled@gmail.com>
2013-09-21 11:40:01 +0800

    Demonstrate manual boxing and unboxing with a type wrapper
    http://ideone.com/iNuuoj

---
+++ Wrap.java
@@ -0,0 +1,10 @@
+// Demonstrate manual boxing and unboxing with a type wrapper.
+class Wrap {
+  public static void main(String args[]) {
+    
+    Integer iOb = new Integer(100);
+    
+    int i = iOb.intValue();
+    System.out.println(i + " " + iOb); // displays 100 100
+  }
+}
